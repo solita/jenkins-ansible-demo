c55c1304479bebb30a53f9666e2664f44ce14f23
Timo Mihaljov <timo.mihaljov@solita.fi>
2017-01-11 13:28:11 +0200

    Add some comments to Groovy files

--- jobs/Hello.groovy
+++ jobs/Hello.groovy
@@ -1,3 +1,10 @@
+// This file is written in Job DSL.  See
+// https://wiki.jenkins-ci.org/display/JENKINS/Job+DSL+Plugin for an overview
+// and https://jenkinsci.github.io/job-dsl-plugin/ for a reference.
+//
+// This file must be directly in the jobs directory. If you move it to a
+// subdirectory, it will be treated as a helper file and not a Job DSL script.
+
 pipelineJob('hello') {
     definition {
         cps {
